db9da3510b17433c4c50cc4fcce7a63b06b6a589
Saketh Reddy <Sakethreddy1998@gmail.com>
2022-08-27 12:07:56 +0530

    added new commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
+import Article from './components/Article';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+
+  useEffect(() =>{
+    fetch("https://www.reddit.com/r/"+ subreddit +".json").then(res =>{
+      if(res.status != 200){
+        console.log("ERROER");
+        return;
+      }
+
+      res.json().then(data => {
+        if(data != null){
+          setArticles(data.data.children);
+        }
+      });
+    })
+
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <input type="text" className="input" value={subreddit}  onChange={e => setSubreddit(e.target.value)}/>
       </header>
+
+      <div className="articles">
+       {
+       (articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''}
+      </div>
     </div>
   );
 }
